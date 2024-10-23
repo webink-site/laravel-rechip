20690d168e42da0314a10e28430d773936eec3b9
Daniil Kondratenko <danikkon2001@gmail.com>
2024-10-23 14:30:50 +0300

    Telegram callbacks for messages

--- app/Http/Controllers/Api/TelegramRequestController.php
+++ app/Http/Controllers/Api/TelegramRequestController.php
@@ -62,16 +62,21 @@ class TelegramRequestController extends Controller
                     ]
                 ];
 
-                $sendStatus = $this->telegram->sendMessage([
-                    'chat_id' => $manager->user_id,
-                    'text' => $text,
-                    'parse_mode' => 'HTML',
-                    'reply_markup' => json_encode($keyboard),
-                ]);
+                try {
+                    $sendStatus = $this->telegram->sendMessage([
+                        'chat_id' => $manager->user_id,
+                        'text' => $text,
+                        'parse_mode' => 'HTML',
+                        'reply_markup' => json_encode($keyboard),
+                    ]);
 
-                if (!$sendStatus->getOk()) {
+                    if (!$sendStatus->messageId) {
+                        $telegramRequest->update(['status' => 'error']);
+                        return response()->json(['error' => 'Ошибка отправки сообщения'], 500);
+                    }
+                }catch (\Exception $e){
                     $telegramRequest->update(['status' => 'error']);
-                    return response()->json(['error' => 'Ошибка отправки сообщения'], 500);
+                    return response()->json(['error' => 'Ошибка отправки сообщения', 'description' => $e->getMessage()], 500);
                 }
             }
         } else {
@@ -79,6 +84,63 @@ class TelegramRequestController extends Controller
         }
 
         return response()->json(['status' => 'success'], 200);
+    }
+
+    public function handleCallback(Request $request)
+    {
+        $callbackQuery = $request->input('callback_query');
+        if (isset($callbackQuery)) {
+            $data = $callbackQuery['data'];
+            $chatId = $callbackQuery['message']['chat']['id'];
+            $messageId = $callbackQuery['message']['message_id'];
+
+            if (strpos($data, 'spam_') === 0) {
+                $requestId = intval(str_replace('spam_', '', $data));
+                $this->handleSpamRequest($requestId, $chatId, $messageId);
+            } elseif (strpos($data, 'completed_') === 0) {
+                $requestId = intval(str_replace('completed_', '', $data));
+                $this->handleCompletedRequest($requestId, $chatId, $messageId);
+            }
+        }
+    }
+
+    private function handleSpamRequest($requestId, $chatId, $messageId)
+    {
+        $telegramRequest = TelegramRequest::find($requestId);
+        if ($telegramRequest) {
+            $telegramRequest->status = 'spam';
+            $telegramRequest->save();
+
+            // Удаление сообщения
+            $this->telegram->deleteMessage([
+                'chat_id' => $chatId,
+                'message_id' => $messageId,
+            ]);
+        }
+    }
+
+    private function handleCompletedRequest($requestId, $chatId, $messageId)
+    {
+        $telegramRequest = TelegramRequest::find($requestId);
+        if ($telegramRequest) {
+            $telegramRequest->status = 'completed';
+            $telegramRequest->save();
+
+            // Обновление сообщения
+            $text = "<b>Контакт: </b>" . e($telegramRequest->contact) . "\n";
+            if ($telegramRequest->product) {
+                $text .= "<b>Продукт: </b>" . e($telegramRequest->product) . "\n";
+            }
+            $text .= e($telegramRequest->request_data);
+            $message = "✅ <b>Заявка #{$requestId} отработана!</b>\n" . $text;
+
+            $this->telegram->editMessageText([
+                'chat_id' => $chatId,
+                'message_id' => $messageId,
+                'text' => $message,
+                'parse_mode' => 'HTML',
+            ]);
+        }
     }
 
     private function decodeUnicode($str)
--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,7 @@ Route::get('/page', [PageController::class, 'index']);
 Route::get('/autos', [AutoController::class, 'handle']);
 
 Route::post('/telegram-requests/submit', [TelegramRequestController::class, 'submit']);
+Route::post('/telegram-requests/callback', [TelegramRequestController::class, 'handleCallback']);
+
 Route::get('/avito/reviews', [AvitoReviewController::class, 'getReviews']);
 
